821e65ed4fa3f5e969809f44ea3d2f346f06bf5e
productoraear <productoraear@gmail.com>
2026-08-13 18:04:21 +0200

    fix(router): erradicacion definitiva de duplicados js mediante LiteralPath

--- src/app/(public)/artists/[slug]/page.js
+++
@@ -1,3 +0,0 @@
-import ArtistProfilePage, { generateMetadata, generateStaticParams } from '../../artistas/[slug]/page';
-export { generateMetadata, generateStaticParams };
-export default ArtistProfilePage;
