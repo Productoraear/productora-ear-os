7bd06e3892d0a3e553b72dbc62dd16c55d22ebdf
productoraear <productoraear@gmail.com>
2026-08-16 08:26:51 +0200

    feat(sclass): integrate high-capacity modules, rescue SEO database and lock high-contrast S-Class theme

---
+++ src/modules/SClassScreens/MatchAsistido.tsx
@@ -0,0 +1,106 @@
+ets/svg/optimized/_common/angleDown.svg" data-svg-lazyload="1"></i>                    </button>
+<div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+<div itemprop="text"
+class="seoFaqsAccordion__itemContent ">
+Eventos La Magrana sirve en sus eventos cocina:                                                             <ul class="seoFaqs__list">
+<li class="seoFaqs__itemList">
+<i class="svgIcon app-svg-async svgIcon__checkLight "   data-svg="https://cdn1.bodas.net/assets/svg/optimized/_common/checkLight.svg" data-svg-lazyload="1"></i>                                            De mercado                                        </li>
+<li class="seoFaqs__itemList">
+<i class="svgIcon app-svg-async svgIcon__checkLight "   data-svg="https://cdn1.bodas.net/assets/svg/optimized/_common/checkLight.svg" data-svg-lazyload="1"></i>                                            De autor                                        </li>
+<li class="seoFaqs__itemList">
+<i class="svgIcon app-svg-async svgIcon__checkLight "   data-svg="https://cdn1.bodas.net/assets/svg/optimized/_common/checkLight.svg" data-svg-lazyload="1"></i>                                            Otros (Mediterránea.)                                        </li>
+</ul>
+</div>
+</div>
+</div>
+<div itemscope itemprop="mainEntity"
+itemtype="https://schema.org/Question"
+class="seoFaqsAccordion__item app-storefront-seofaqs-list-item">
+<button class="seoFaqsAccordion__itemHeader" type="button">
+<span itemprop="name"
+class="seoFaqsAccordion__itemHeaderTitle">¿Qué tipo de menús especiales para boda es posible encontrar en Eventos La Magrana?</span>
+<i class="svgIcon app-svg-async svgIcon__angleDown seoFaqsAccordion__itemHeaderIcon"   data-svg="https://cdn1.bodas.net/assets/svg/optimized/_common/angleDown.svg" data-svg-lazyload="1"></i>                    </button>
+<div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+<div itemprop="text"
+class="seoFaqsAccordion__itemContent ">
+Eventos La Magrana cuenta con diversos menús especiales de boda entre los que destacan:                                                             <ul class="seoFaqs__list">
+<li class="seoFaqs__itemList">
+<i class="svgIcon app-svg-async svgIcon__checkLight "   data-svg="https://cdn1.bodas.net/assets/svg/optimized/_common/checkLight.svg" data-svg-lazyload="1"></i>                                            Vegetarianos                                        </li>
+<li class="seoFaqs__itemList">
+<i class="svgIcon app-svg-async svgIcon__checkLight "   data-svg="https://cdn1.bodas.net/assets/svg/optimized/_common/checkLight.svg" data-svg-lazyload="1"></i>                                            Veganos                                        </li>
+<li class="seoFaqs__itemList">
+<i class="svgIcon app-svg-async svgIcon__checkLight "   data-svg="https://cdn1.bodas.net/assets/svg/optimized/_common/checkLight.svg" data-svg-lazyload="1"></i>                                            Diabéticos                                        </li>
+<li class="seoFaqs__itemList">
+<i class="svgIcon app-svg-async svgIcon__checkLight "   data-svg="https://cdn1.bodas.net/assets/svg/optimized/_common/checkLight.svg" data-svg-lazyload="1"></i>                                            Hipertensos                                        </li>
+<li class="seoFaqs__itemList">
+<i class="svgIcon app-svg-async svgIcon__checkLight "   data-svg="https://cdn1.bodas.net/assets/svg/optimized/_common/checkLight.svg" data-svg-lazyload="1"></i>                                            Alérgicos                                        </li>
+<li class="seoFaqs__itemList">
+<i class="svgIcon app-svg-async svgIcon__checkLight "   data-svg="https://cdn1.bodas.net/assets/svg/optimized/_common/checkLight.svg" data-svg-lazyload="1"></i>                                            Celíacos                                        </li>
+</ul>
+</div>
+</div>
+</div>
+<div itemscope itemprop="mainEntity"
+itemtype="https://schema.org/Question"
+class="seoFaqsAccordion__item app-storefront-seofaqs-list-item">
+<button class="seoFaqsAccordion__itemHeader" type="button">
+<span itemprop="name"
+class="seoFaqsAccordion__itemHeaderTitle">¿Cómo funciona la contratación de la barra libre para bodas en Eventos La Magrana?</span>
+<i class="svgIcon app-svg-async svgIcon__angleDown seoFaqsAccordion__itemHeaderIcon"   data-svg="https://cdn1.bodas.net/assets/svg/optimized/_common/angleDown.svg" data-svg-lazyload="1"></i>                    </button>
+<div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+<div itemprop="text"
+class="seoFaqsAccordion__itemContent ">
+Eventos La Magrana ofrece las siguientes opciones para la contratación de la barra libre:                                                             <ul class="seoFaqs__list">
+<li class="seoFaqs__itemList">
+<i class="svgIcon app-svg-async svgIcon__checkLight "   data-svg="https://cdn1.bodas.net/assets/svg/optimized/_common/checkLight.svg" data-svg-lazyload="1"></i>                                            Cantidad total fija (con límite de hora)                                        </li>
+</ul>
+</div>
+</div>
+</div>
+<div itemscope itemprop="mainEntity"
+itemtype="https://schema.org/Question"
+class="seoFaqsAccordion__item app-storefront-seofaqs-list-item">
+<button class="seoFaqsAccordion__itemHeader" type="button">
+<span itemprop="name"
+class="seoFaqsAccordion__itemHeaderTitle">¿A cuántas personas puede dar servicio Eventos La Magrana para la celebración del banquete de boda?</span>
+<i class="svgIcon app-svg-async svgIcon__angleDown seoFaqsAccordion__itemHeaderIcon"   data-svg="https://cdn1.bodas.net/assets/svg/optimized/_common/angleDown.svg" data-svg-lazyload="1"></i>                    </button>
+<div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+<div itemprop="text"
+class="seoFaqsAccordion__itemContent ">
+Eventos La Magrana ofrece sus servicios de banquete de boda para un mínimo de 120 invitados y puede dar servicio hasta un máximo de 480 invitados.                                                    </div>
+</div>
+</div>
+<div itemscope itemprop="mainEntity"
+itemtype="https://schema.org/Question"
+class="seoFaqsAccordion__item app-storefront-seofaqs-list-item">
+<button class="seoFaqsAccordion__itemHeader" type="button">
+<span itemprop="name"
+class="seoFaqsAccordion__itemHeaderTitle">¿Cuántas parejas recomiendan a Eventos La Magrana y cuáles son los aspectos mejor valorados de sus servicios para bodas?</span>
+<i class="svgIcon app-svg-async svgIcon__angleDown seoFaqsAccordion__itemHeaderIcon"   data-svg="https://cdn1.bodas.net/assets/svg/optimized/_common/angleDown.svg" data-svg-lazyload="1"></i>                    </button>
+<div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+<div itemprop="text"
+class="seoFaqsAccordion__itemContent ">
+A Eventos La Magrana lo recomienda el 91% de las parejas que ya disfrutó de sus servicios. Su valoración global es de 4.6 sobre 5 que se desglosa del siguiente modo: 4.7 en calidad del servicio, 4.5 en flexibilidad, 4.5 en profesionalidad, 4.5 en relación calidad/precio y 4.4 en tiempo de respuesta.                                                    </div>
+</div>
+</div>
+</div>
+</section>
+</div>
+<aside class="storefrontAside app-article-storefront-aside">
+<div class="storefrontAside__sticky storefrontFormAside app-storefront-aside-sticky storefrontAside__stickyWithMargin storefrontFormAside--hide">
+
+    <section class="paidVendorsAside  app-sponsored-vendors-aside app-storefront-aside-sticky-content">
+        <h2 class="paidVendorsAside__title app-sponsored-placements-aside-title">Sigue buscando</h2>
+        <div class="paidVendorsAside__main app-sponsored-placements-aside app-vendor-list-tracking-impressions"
+             data-product-listing-type="Directory: Continue Browsing"
+             data-sort-type=""
+             data-category-group-id="1"
+             data-category-id="1"
+             data-region-id="3004"
+             data-region-adm1-id="144"
+             data-city-id="805307"
+        >
+                                        
+<div class="paidVendorTile app-vendor-tile paidVendorTile--aside app-sponsored-placement-tile" data-vendor-id="240384"
+     data-vendor-uuid="afc24c8b-656f-4d44-9997-21433cb493ee"
+   
---
+++ src/modules/SClassScreens/PaymentModal.tsx
@@ -0,0 +1,78 @@
+0"
+width="640"        height="427"                loading="lazy">
+</picture>
+<figcaption>Horquilla 7060</figcaption>
+</figure>
+<figure class="storefrontMultiGallery__item app-scroll-snap-item app-gallery-image-fullscreen-open app-gallery-global-tracking " data-type="image" data-type-id="65">
+<picture      data-image-name="imageFileName_7261_1_6598-166678249863093.jpeg">
+<source
+type="image/webp"
+srcset="https://cdn0.bodas.net/vendor/6598/3_2/320/jpg/7261_1_6598-166678249863093.webp 320w,
+https://cdn0.bodas.net/vendor/6598/3_2/640/jpg/7261_1_6598-166678249863093.webp 640w,
+https://cdn0.bodas.net/vendor/6598/3_2/960/jpg/7261_1_6598-166678249863093.webp 960w,
+https://cdn0.bodas.net/vendor/6598/3_2/1280/jpg/7261_1_6598-166678249863093.webp 1280w,
+https://cdn0.bodas.net/vendor/6598/3_2/1920/jpg/7261_1_6598-166678249863093.webp 1920w" sizes="(min-width: 1024px) 600px, (min-width: 480px) 400px, 100vw">
+<img
+srcset="https://cdn0.bodas.net/vendor/6598/3_2/320/jpg/7261_1_6598-166678249863093.jpeg 320w,
+https://cdn0.bodas.net/vendor/6598/3_2/640/jpg/7261_1_6598-166678249863093.jpeg 640w,
+https://cdn0.bodas.net/vendor/6598/3_2/960/jpg/7261_1_6598-166678249863093.jpeg 960w,
+https://cdn0.bodas.net/vendor/6598/3_2/1280/jpg/7261_1_6598-166678249863093.jpeg 1280w,
+https://cdn0.bodas.net/vendor/6598/3_2/1920/jpg/7261_1_6598-166678249863093.jpeg 1920w"
+src="https://cdn0.bodas.net/vendor/6598/3_2/960/jpg/7261_1_6598-166678249863093.jpeg"
+sizes="(min-width: 1024px) 600px, (min-width: 480px) 400px, 100vw"
+alt="Horquilla 7061"
+width="640"        height="427"                loading="lazy">
+</picture>
+<figcaption>Horquilla 7061</figcaption>
+</figure>
+<figure class="storefrontMultiGallery__item app-scroll-snap-item app-gallery-image-fullscreen-open app-gallery-global-tracking " data-type="image" data-type-id="66">
+<picture      data-image-name="imageFileName_7172-2_1_6598-166678288686324.jpeg">
+<source
+type="image/webp"
+srcset="https://cdn0.bodas.net/vendor/6598/3_2/320/jpg/7172-2_1_6598-166678288686324.webp 320w,
+https://cdn0.bodas.net/vendor/6598/3_2/640/jpg/7172-2_1_6598-166678288686324.webp 640w,
+https://cdn0.bodas.net/vendor/6598/3_2/960/jpg/7172-2_1_6598-166678288686324.webp 960w,
+https://cdn0.bodas.net/vendor/6598/3_2/1280/jpg/7172-2_1_6598-166678288686324.webp 1280w,
+https://cdn0.bodas.net/vendor/6598/3_2/1920/jpg/7172-2_1_6598-166678288686324.webp 1920w" sizes="(min-width: 1024px) 600px, (min-width: 480px) 400px, 100vw">
+<img
+srcset="https://cdn0.bodas.net/vendor/6598/3_2/320/jpg/7172-2_1_6598-166678288686324.jpeg 320w,
+https://cdn0.bodas.net/vendor/6598/3_2/640/jpg/7172-2_1_6598-166678288686324.jpeg 640w,
+https://cdn0.bodas.net/vendor/6598/3_2/960/jpg/7172-2_1_6598-166678288686324.jpeg 960w,
+https://cdn0.bodas.net/vendor/6598/3_2/1280/jpg/7172-2_1_6598-166678288686324.jpeg 1280w,
+https://cdn0.bodas.net/vendor/6598/3_2/1920/jpg/7172-2_1_6598-166678288686324.jpeg 1920w"
+src="https://cdn0.bodas.net/vendor/6598/3_2/960/jpg/7172-2_1_6598-166678288686324.jpeg"
+sizes="(min-width: 1024px) 600px, (min-width: 480px) 400px, 100vw"
+alt="Tiara 7172"
+width="640"        height="427"                loading="lazy">
+</picture>
+<figcaption>Tiara 7172</figcaption>
+</figure>
+<figure class="storefrontMultiGallery__item app-scroll-snap-item app-gallery-image-fullscreen-open app-gallery-global-tracking " data-type="image" data-type-id="67">
+<picture      data-image-name="imageFileName_7178_1_6598-166678288659491.jpeg">
+<source
+type="image/webp"
+srcset="https://cdn0.bodas.net/vendor/6598/3_2/320/jpg/7178_1_6598-166678288659491.webp 320w,
+https://cdn0.bodas.net/vendor/6598/3_2/640/jpg/7178_1_6598-166678288659491.webp 640w,
+https://cdn0.bodas.net/vendor/6598/3_2/960/jpg/7178_1_6598-166678288659491.webp 960w,
+https://cdn0.bodas.net/vendor/6598/3_2/1280/jpg/7178_1_6598-166678288659491.webp 1280w,
+https://cdn0.bodas.net/vendor/6598/3_2/1920/jpg/7178_1_6598-166678288659491.webp 1920w" sizes="(min-width: 1024px) 600px, (min-width: 480px) 400px, 100vw">
+<img
+srcset="https://cdn0.bodas.net/vendor/6598/3_2/320/jpg/7178_1_6598-166678288659491.jpeg 320w,
+https://cdn0.bodas.net/vendor/6598/3_2/640/jpg/7178_1_6598-166678288659491.jpeg 640w,
+https://cdn0.bodas.net/vendor/6598/3_2/960/jpg/7178_1_6598-166678288659491.jpeg 960w,
+https://cdn0.bodas.net/vendor/6598/3_2/1280/jpg/7178_1_6598-166678288659491.jpeg 1280w,
+https://cdn0.bodas.net/vendor/6598/3_2/1920/jpg/7178_1_6598-166678288659491.jpeg 1920w"
+src="https://cdn0.bodas.net/vendor/6598/3_2/960/jpg/7178_1_6598-166678288659491.jpeg"
+sizes="(min-width: 1024px) 600px, (min-width: 480px) 400px, 100vw"
+alt="Peineta 7178"
+width="640"        height="427"                loading="lazy">
+</picture>
+<figcaption>Peineta 7178</figcaption>
+</figure>
+<figure class="storefrontMultiGallery__item app-scroll-snap-item app-gallery-image-fullscreen-open app-gallery-global-tracking " data-type="image" data-type-id="68">
+<picture      data-image-name="imageFileName_7269_1_6598-166678288687973.jpeg">
+<source
+type="image/webp"
+srcset="https://cdn0.bodas.net/vendor/6598/3_2/320/jpg/7269_1_6598-166678288687973.webp 320w,
+https://cdn0.bodas.net/vendor/6598/3_2/640/jpg/7269_1_6598-166678288687973.webp 640w,
+https://cdn0.bodas.net/vendor/6598/3_2/960/jpg/7269_1_6598-166
